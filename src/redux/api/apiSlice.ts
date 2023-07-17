b931df0117b38da795096e1999ceb44f3900cd6b
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-07-17 18:02:47 +0600

    Production URL updated

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5001/",
+    baseUrl: "https://book-vault-server.vercel.app/",
   }),
   tagTypes: ["Books", "Book", "Reviews", "Wishlist", "Booklist"],
   endpoints: () => ({}),
